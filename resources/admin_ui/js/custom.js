693f7c3b3b2aabf52afd55ebf029e667624881f4
zanabazard <64764014+zanabazard@users.noreply.github.com>
2021-10-05 09:55:09 +0800

    contact us submit

--- resources/admin_ui/js/custom.js
+++ resources/admin_ui/js/custom.js
@@ -7,36 +7,36 @@ import Axios from "axios"
 // })
 
 // Form disabler Start
-//     $('.disabler').on('click', ()=>{
-//         if ($('#ContactUsForm fieldset').attr('disabled') === 'disabled'){
-//             $('#ContactUsForm fieldset').removeAttr('disabled')
-//             $('#btns').append('<button type="button" class="mt-2 btn btn-success contactSubmit contactSave">Save</button>')
-//         }else {
-//             $('#ContactUsForm fieldset').attr('disabled','disabled')
-//             $('.contactSubmit').remove()
-//         }
-//     })
+    $('.disabler').on('click', ()=>{
+        if ($('#ContactUsForm fieldset').attr('disabled') === 'disabled'){
+            $('#ContactUsForm fieldset').removeAttr('disabled')
+            $('#btns').append('<button type="button" class="mt-2 btn btn-success contactSubmit contactSave">Save</button>')
+        }else {
+            $('#ContactUsForm fieldset').attr('disabled','disabled')
+            $('.contactSubmit').remove()
+        }
+    })
 // Form disabler End
 
 // contact us submit START
-// $(document).on('click',()=>{
-//     $('.contactSubmit').on('click', () => {
-//         const data = {
-//             title: $('#title').val(),
-//             email: $('#email').val(),
-//             phone:$('#phone').val(),
-//             short_content: $('#short_content').val(),
-//             address:$('#address').val(),
-//             facebook:$('#facebook').val(),
-//             youtube:$('#youtube').val(),
-//             twitter:$('#twitter').val(),
-//             linkedin:$("#linkedin").val()
-//         }
-//         Axios.post('contact_us', data).then((resp)=>{
-//             console.log(resp);
-//         }).catch((err)=>{
-//             alert('error')
-//         })
-//     })
-// });
+$(document).on('click',()=>{
+    $('.contactSubmit').on('click', () => {
+        const data = {
+            title: $('#title').val(),
+            email: $('#email').val(),
+            phone:$('#phone').val(),
+            short_content: $('#short_content').val(),
+            address:$('#address').val(),
+            facebook:$('#facebook').val(),
+            youtube:$('#youtube').val(),
+            twitter:$('#twitter').val(),
+            linkedin:$("#linkedin").val()
+        }
+        Axios.post('contact_us', data).then((resp)=>{
+            console.log(resp);
+        }).catch((err)=>{
+            alert('error')
+        })
+    })
+});
 // contact us submit END
